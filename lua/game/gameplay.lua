ab0fac2d14ed065e1acd41c2079d440aabb9e4f7
julien DEVILLE <julien.deville@gmail.com>
2013-03-20 17:35:58 +0100

    ajout lvl 9 et suppression du theme musical, désolé

--- lua/game/gameplay.lua
+++ lua/game/gameplay.lua
@@ -30,7 +30,7 @@ require("const")
 
         --Map
         --self.mapLoader = MapLoader.new("maps.map1",self.magnetmanager)
-        self.mapLoader = MapLoader.new("maps.level6",self.magnetmanager)
+        self.mapLoader = MapLoader.new("maps.level9",self.magnetmanager)
 
         -- Camera Metal Man
         self.cameraMM =Camera.new(0,0)
