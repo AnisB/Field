bd38a728f6d0f3c24cf3a1559c7579bddbd72115
Anis <Benyoub.anis@gmail.com>
2013-04-07 19:42:27 +0200

    lol

--- lua/client/game/inputmanager.lua
+++ lua/client/game/inputmanager.lua
@@ -16,38 +16,39 @@ function InputManager.new()
 end
 
 function InputManager:keyPressed(akey, unicode)
-  print("J'ai un input à handler"..akey)
   if(monde.moi.perso=="metalman") then 
-    print("C'est un metalman, recherche de la clé")
     for i,v in pairs(self.keys.MetalManKEYS) do
       if(v==akey) then
-        print("Trouvée"..i)
-        serveur:send({type="input", pck={character="metalMan", key=MetalManKEYS.i, state=true}})
+        serveur:send({type="input", pck={character="metalman", key=MetalManKEYS[i], state=true}})
       end
     end
-    print("Pas trouvée"..akey)
     return
     elseif (monde.moi.perso=="themagnet")then
-      for i,v in pairs(self.keys.MetalManKEYS) do
+      for i,v in pairs(self.keys.TheMagnetKEYS) do
         if(v==akey) then
-          serveur:send({type="input", pck={character="themagnet", key=MetalManKEYS.i, state=true}})
+          serveur:send({type="input", pck={character="themagnet", key=TheMagnetKEYS[i], state=true}})
         end
       end
     end
   end
 
-  function InputManager:keyReleased(key, unicode)
+  function InputManager:keyReleased(akey, unicode)
+  print("J'ai un input à handler"..akey)
   if(monde.moi.perso=="metalman") then 
+    print("C'est un metalman, recherche de la clé")
+
     for i,v in pairs(self.keys.MetalManKEYS) do
       if(v==akey) then
-        serveur:send({type="input", pck={character="metalMan", key=MetalManKEYS.i, state=false}})
+        print("Trouvée"..i.."#"..v.."Envoyé"..MetalManKEYS[i])
+        serveur:send({type="input", pck={character="metalman", key=MetalManKEYS[i], state=false}})
       end
     end
+    print("Pas trouvée"..akey)
     return
     elseif (monde.moi.perso=="themagnet")then
-      for i,v in pairs(self.keys.MetalManKEYS) do
+      for i,v in pairs(self.keys.TheMagnetKEYS) do
         if(v==akey) then
-          serveur:send({type="input", pck={character="themagnet", key=MetalManKEYS.i, state=false}})
+          serveur:send({type="input", pck={character="themagnet", key=TheMagnetKEYS[i], state=false}})
         end
       end
     end
