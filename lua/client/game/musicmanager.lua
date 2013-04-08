752982c65b3ba19662e9bfc710722f0e96326ee5
Anis <Benyoub.anis@gmail.com>
2013-04-08 14:26:16 +0200

    musci

---
+++ lua/client/game/musicmanager.lua
@@ -0,0 +1,44 @@
+MusicManager = {}
+MusicManager.__index = MusicManager
+
+MusicManager.SOUND_ROOT = "sound/"
+MusicManager.SOUND_LOOP = ""--"loop"
+
+MUSIC_VOL = 10
+
+
+function MusicManager.new()
+    local self = {}
+    setmetatable(self, MusicManager)
+	
+	local introPath = MusicManager.SOUND_ROOT.."theme"..'.ogg'
+	local mainLoopPath = MusicManager.SOUND_ROOT.."theme"..'.ogg'
+	self.srcIntro = love.audio.newSource(introPath, "static")
+	self.srcMainLoop = love.audio.newSource(mainLoopPath, "static")
+
+	self.introIsDone = false
+    return self
+end
+
+function MusicManager:play()
+	self.isPlaying = true
+	self.introIsDone =false
+	self.srcIntro:play()
+	self.srcIntro:setVolume(MUSIC_VOL)
+	self.srcIntro:setLooping(false)
+end
+
+function MusicManager:update(dt)
+	if self.isPlaying then
+		if self.srcIntro:isStopped()  and not self.introIsDone then
+			self.introIsDone =true
+			if self.isStopped~=true then
+				if self.srcLoop:isStopped() then
+					self.srcMainLoop:setLooping(true)
+					self.srcMainLoop:play()
+					self.srcMainLoop:setVolume(MUSIC_VOL)
+				end
+			end
+		end
+	end
+end
