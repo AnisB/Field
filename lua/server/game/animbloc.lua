42c917d0ab68a8762f501ddb75ca65d0ff4ea6f3
Anis Benyoub <benyoub.anis@gmail.com>
2013-07-09 12:22:51 +0200

    Update animbloc.lua

--- lua/server/game/animbloc.lua
+++ lua/server/game/animbloc.lua
@@ -104,7 +104,7 @@ end
 
 -- PRIVATE
 function AnimBloc:updateImg()
-	self.currentImg = self.sprites[self.currentAnim.name][self.currentPos]
+	--self.currentImg = self.sprites[self.currentAnim.name][self.currentPos]
 end
 
 -- NETWORK
