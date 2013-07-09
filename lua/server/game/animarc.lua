a9be6d37eaf2b0141023245614dcd00a937c4a0e
Anis Benyoub <benyoub.anis@gmail.com>
2013-07-09 12:22:04 +0200

    Update animarc.lua

--- lua/server/game/animarc.lua
+++ lua/server/game/animarc.lua
@@ -119,7 +119,7 @@ end
 
 -- PRIVATE
 function AnimArc:updateImg()
-	self.currentImg = self.sprites[self.currentAnim.name][self.currentPos]
+	--self.currentImg = self.sprites[self.currentAnim.name][self.currentPos]
 end
 
 -- NETWORK
